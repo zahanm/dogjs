8563bcd42525527db66d7d4e9b70414d8e3fec0e
Zahan Malkani <zahanm@gmail.com>
2012-06-25 17:18:11 -0400

    [fakeserver] responses as json

--- examples/hellow.js
+++ examples/hellow.js
@@ -1,5 +1,5 @@
 
-var express, server;
+var express, server, port;
 
 express = require('express');
 
@@ -7,12 +7,29 @@ server = express.createServer();
 
 server.configure(function() {
   server.use(express.logger('short'));
+  server.use(jsonType);
+  server.use(server.router);
 });
 
 server.get('/', function(req, res) {
-  res.send('Hello world');
+  res.send(json('Hello world'));
 });
 
-server.listen(process.env.PORT || 8080, function() {
-  console.log('Listening');
+port = process.env.PORT || 8080;
+server.listen(port, function() {
+  console.log('Listening on', port);
 });
+
+// Helpers
+
+function jsonType (req, res, next) {
+  res.contentType('application/json');
+  next();
+}
+
+function json (o) {
+  if (typeof o === 'string') {
+    o = { 'msg': o };
+  }
+  return JSON.stringify(o);
+}
