32958e43a14f9a4824465484838418787893792c
Zahan Malkani <zahanm@gmail.com>
2012-06-28 15:59:39 -0400

    EventEmitter, used in Request and dogjs

--- dog.js
+++ dog.js
@@ -1,3 +1,34 @@
+// ### EventEmitter
+//
+// Emits events, and can be inherited from.
+//
+(function(exports) {
+
+  function EventEmitter() {
+    this.listeners = {};
+    return this;
+  }
+
+  EventEmitter.prototype.on = function(eventname, callback, context) {
+    if ( !(eventname in this.listeners) ) {
+      this.listeners[ eventname ] = [];
+    }
+    this.listeners[ eventname ].push({ f: callback, c: context || this });
+  };
+
+  EventEmitter.prototype.emit = function(eventname) {
+    if (eventname in this.listeners) {
+      var args = Array.prototype.slice.call(arguments);
+      this.listeners[ eventname ].forEach(function(listener) {
+        listener.f.apply( listener.c, args.slice(1) );
+      }, this);
+    }
+  };
+
+  exports.EventEmitter = EventEmitter;
+
+})(window);
+
 // ### Request
 //
 // Request is a simplification of AJAX requests made through `XMLHttpRequest`
@@ -21,10 +52,7 @@
 //
 (function(exports) {
 
-  var Request;
-
-  Request = function() {
-    this.listeners = {};
+  function Request() {
     this.xhp = new XMLHttpRequest();
     var reqcontext = this;
     this.xhp.addEventListener("load", function() {
@@ -34,7 +62,10 @@
       errored.apply(reqcontext, arguments);
     });
     return this;
-  };
+  }
+  // inherit from EventEmitter
+  Request.prototype = new EventEmitter();
+  Request.prototype.constructor = Request;
   exports.Request = Request;
 
   Request.prototype.get = function(url) {
@@ -42,24 +73,13 @@
     this.xhp.send();
   };
 
-  Request.prototype.on = function(eventname, callback, context) {
-    if ( !(eventname in this.listeners) ) {
-      this.listeners[ eventname ] = [];
-    }
-    this.listeners[ eventname ].push({ f: callback, c: context || this });
-  };
-
   function loaded (ev) {
     if ( this.xhp.status.toString()[0] === '2' ) {
-      if ('success' in this.listeners) {
-        var data = null;
-        try {
-          data = JSON.parse(this.xhp.responseText);
-        } catch (err) { }
-        this.listeners['success'].forEach(function(listener) {
-          listener.f.call(listener.c, data || this.xhp.responseText, this.xhp.statusText);
-        }, this);
-      }
+      var data = null;
+      try {
+        data = JSON.parse(this.xhp.responseText);
+      } catch (err) { }
+      this.emit('success', data || this.xhp.responseText, this.xhp.statusText);
     } else {
       console.log('Successful request with non-2XX status.');
       console.log(this.xhp);
@@ -67,11 +87,7 @@
   }
 
   function errored (ev) {
-    if ('error' in this.listeners) {
-      this.listeners['error'].forEach(function(listener) {
-        listener.f.call(listener.c, this.xhp.statusText);
-      }, this);
-    }
+    this.emit('error', this.xhp.statusText);
     console.log('Unsuccessful request.');
     console.log(this.xhp);
   }
@@ -86,12 +102,8 @@
 //
 (function(exports) {
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var request = new JaxLib.Request();
-    request.on('success', function(data) {
-      console.log( data['msg'] );
-    });
-    request.get('http://localhost:8080/helloworld');
-  }, false);
+  // dogjs is an augmented EventEmitter instance
+  var dogjs = new EventEmitter();
+  exports.dogjs = dogjs;
 
-})(window.dogjs = {});
+})(window);
