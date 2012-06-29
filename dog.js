181f782aa771c1fa5d264c9eb87c898d16075609
Zahan Malkani <zahanm@gmail.com>
2012-06-29 18:41:26 -0400

    using jslint for code

--- dog.js
+++ dog.js
@@ -2,32 +2,33 @@
 //
 // Emits events, and can be inherited from.
 //
-(function(exports) {
+(function (exports) {
+  'use strict';
 
   function EventEmitter() {
     this.listeners = {};
     return this;
   }
 
-  EventEmitter.prototype.on = function(eventname, callback, context) {
-    if ( !(eventname in this.listeners) ) {
-      this.listeners[ eventname ] = [];
+  EventEmitter.prototype.on = function (eventname, callback, context) {
+    if (this.listeners[eventname] === undefined) {
+      this.listeners[eventname] = [];
     }
-    this.listeners[ eventname ].push({ f: callback, c: context || this });
+    this.listeners[eventname].push({ f: callback, c: context || this });
   };
 
-  EventEmitter.prototype.emit = function(eventname) {
-    if (eventname in this.listeners) {
+  EventEmitter.prototype.emit = function (eventname) {
+    if (this.listeners[eventname] !== undefined) {
       var args = Array.prototype.slice.call(arguments);
-      this.listeners[ eventname ].forEach(function(listener) {
-        listener.f.apply( listener.c, args.slice(1) );
+      this.listeners[eventname].forEach(function (listener) {
+        listener.f.apply(listener.c, args.slice(1));
       }, this);
     }
   };
 
   exports.EventEmitter = EventEmitter;
 
-})(window);
+}(window));
 
 // ### Request
 //
@@ -50,49 +51,53 @@
 //     request.on('success', ..);
 //     request.get('http://jabber.wocky.org/dog/');
 //
-(function(exports) {
+(function (exports) {
+  'use strict';
+
+  function loaded(req) {
+    if (req.xhp.status.toString()[0] === '2') {
+      var data = null;
+      try {
+        data = JSON.parse(req.xhp.responseText);
+      } catch (err) { }
+      req.emit('success', data || req.xhp.responseText, req.xhp.statusText);
+    } else {
+      console.log('Successful request with non-2XX status.');
+      console.log(req.xhp);
+    }
+  }
+
+  function errored(req) {
+    req.emit('error', req.xhp.statusText);
+    console.log('Unsuccessful request.');
+    console.log(req.xhp);
+  }
 
   function Request() {
     this.xhp = new XMLHttpRequest();
-    var reqcontext = this;
-    this.xhp.addEventListener("load", function() {
-      loaded.apply(reqcontext, arguments);
+    var req = this;
+    this.xhp.addEventListener("load", function () {
+      loaded(req);
     });
-    this.xhp.addEventListener("error", function() {
-      errored.apply(reqcontext, arguments);
+    this.xhp.addEventListener("error", function () {
+      errored(req);
     });
     return this;
   }
   // inherit from EventEmitter
   Request.prototype = new EventEmitter();
   Request.prototype.constructor = Request;
-  exports.Request = Request;
+  Request.prototype.__super__ = EventEmitter.prototype;
 
-  Request.prototype.get = function(url) {
+  Request.prototype.get = function (url) {
     this.xhp.open('GET', url, true);
     this.xhp.send();
   };
 
-  function loaded (ev) {
-    if ( this.xhp.status.toString()[0] === '2' ) {
-      var data = null;
-      try {
-        data = JSON.parse(this.xhp.responseText);
-      } catch (err) { }
-      this.emit('success', data || this.xhp.responseText, this.xhp.statusText);
-    } else {
-      console.log('Successful request with non-2XX status.');
-      console.log(this.xhp);
-    }
-  }
+  // make Request public
+  exports.Request = Request;
 
-  function errored (ev) {
-    this.emit('error', this.xhp.statusText);
-    console.log('Unsuccessful request.');
-    console.log(this.xhp);
-  }
-
-})(window.JaxLib = {});
+}(window));
 
 // ### dogjs
 //
@@ -100,10 +105,11 @@
 //
 // Prereqs: To be used in *modern* browsers
 //
-(function(exports) {
+(function (exports) {
+  'use strict';
 
-  // dogjs is an augmented EventEmitter instance
+  // `dogjs` is an augmented EventEmitter instance
   var dogjs = new EventEmitter();
   exports.dogjs = dogjs;
 
-})(window);
+}(window));
