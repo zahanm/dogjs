ddc7491d2aef01ac7de63ec189ddb2ad1dc6be28
Zahan Malkani <zahanm@gmail.com>
2012-08-16 12:25:49 -0400

    using Promise from @babelon 's lie-to-me/defers.js

--- dog.js
+++ dog.js
@@ -178,40 +178,47 @@
 // ## Promise
 //
 // Using for cleaner asynchronous programming.
-// Uses EventEmitter under the scenes.
+// Taken from [lie-to-me/lib/defers.js](https://github.com/babelon/lie-to-me/)
 //
 (function (exports) {
 
-  function Promise() {
-    this.__super__.constructor.call(this);
-  }
+  function Promise () {
+    this.continuations = [];
+    this.contexts = [];
+    this.stalled = function() {};
+  };
 
-  // inherit from EventEmitter
-  Promise.prototype = new EventEmitter();
-  Promise.prototype.constructor = Promise;
-  Promise.prototype.__super__ = EventEmitter.prototype;
-
-  Promise.prototype.then = function (continuation) {
-    this.on('success', continuation);
+  Promise.prototype.then = function(f, c) {
+    if (typeof f !== 'function') { throw new TypeError("You must pass a function as the 'then' handler"); }
+    this.continuations.push(f);
+    this.contexts.push(c || this);
     return this;
   };
 
-  Promise.prototype.instead = function (errorhandler) {
-    this.on('error', errorhandler);
+  Promise.prototype.instead = function(f) {
+    if (typeof f !== 'function') { throw new TypeError("You must pass a function as the 'error' handler"); }
+    this.stalled = f;
     return this;
   };
 
-  Promise.prototype.resolve = function () {
-    var args = Array.prototype.slice.call(arguments);
-    args.unshift('success');
-    this.emit.apply(this, args);
-  }
+  /**
+    God, this is beautiful.
+    If you add a promise followon using .then(),
+    the next .then() will be invoked after
+    it finishes
+  */
+  Promise.prototype.onward = function onward() {
+    var next, c, ret;
+    if (!this.continuations.length) { return; }
+    next = this.continuations.shift();
+    c = this.contexts.shift();
+    ret = next.apply(c, arguments);
+    if (ret instanceof Promise) {
+      ret.then(this.onward, this);
+    }
+  };
 
-  Promise.prototype.abort = function () {
-    var args = Array.prototype.slice.call(arguments);
-    args.unshift('error');
-    this.emit.apply(this, args);
-  }
+  exports.Promise = Promise;
 
 }(window));
 
